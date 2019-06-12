fca0f6c5b7c3374dfb5fb3329485c0924aa23e65
nick <n.denisov@carbonsoft.ru>
2019-06-13 00:30:39 +0500

    Add(examples): simple UART

--- examples/nrf5-sdk/blinky/main.c
+++ examples/nrf5-sdk/blinky/main.c
@@ -1,4 +1,3 @@
-
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -7,7 +6,7 @@
 #include "nrf_delay.h"
 
 void clock_initialization() {
-    /* Start 16 MHz crystal oscillator */
+    /* Start 32 MHz crystal oscillator */
     NRF_CLOCK->EVENTS_HFCLKSTARTED = 0;
     NRF_CLOCK->TASKS_HFCLKSTART = 1;
 
@@ -15,48 +14,34 @@ void clock_initialization() {
     while (NRF_CLOCK->EVENTS_HFCLKSTARTED == 0) {
         // Do nothing.
     }
-
-    /* Start low frequency crystal oscillator for app_timer(used by bsp)*/
-    NRF_CLOCK->LFCLKSRC = (CLOCK_LFCLKSRC_SRC_Xtal << CLOCK_LFCLKSRC_SRC_Pos);
-    NRF_CLOCK->EVENTS_LFCLKSTARTED = 0;
-    NRF_CLOCK->TASKS_LFCLKSTART = 1;
-
-    while (NRF_CLOCK->EVENTS_LFCLKSTARTED == 0) {
-        // Do nothing.
-    }
 }
 
-void UART_init(void) {
+void uart_init(void) {
     /*Initialize the UART peripheral */
-    NRF_UART0->ENABLE = 0;
-    NRF_UART0->INTENCLR = 0xFFFFFFFF;
+    NRF_UART0->BAUDRATE = UART_BAUDRATE_BAUDRATE_Baud115200;
     NRF_P0->DIRSET = (1 << 19); /*! RXD Input */
     NRF_P0->DIRCLR = (1 << 20); /*! TXD Output */
-    NRF_UART0->PSEL.RXD = (1 << 19);
-    NRF_UART0->PSEL.TXD = (1 << 20);
+    NRF_UART0->PSEL.RXD = 19;
+    NRF_UART0->PSEL.TXD = 20;
     NRF_UART0->PSEL.CTS = 0xFFFFFFFF;
     NRF_UART0->PSEL.RTS = 0xFFFFFFFF;
-    NRF_UART0->BAUDRATE = (UART_BAUDRATE_BAUDRATE_Baud115200 << UART_BAUDRATE_BAUDRATE_Pos);
-    NRF_UART0->CONFIG = (UART_CONFIG_HWFC_Msk << UART_CONFIG_HWFC_Pos);
+    NRF_UART0->CONFIG = (UART_CONFIG_PARITY_Excluded << UART_CONFIG_PARITY_Pos |
+                         UART_CONFIG_HWFC_Disabled << UART_CONFIG_HWFC_Pos |
+                         UART_CONFIG_STOP_One << UART_CONFIG_STOP_Pos);
     NRF_UART0->ENABLE = (UART_ENABLE_ENABLE_Enabled << UART_ENABLE_ENABLE_Pos);
-    NRF_UART0->CONFIG |= (UART_CONFIG_PARITY_Msk << UART_CONFIG_PARITY_Pos);
     NRF_UART0->TASKS_STARTTX = 1;
-    NRF_UART0->TASKS_STARTRX = 1;
-    return;
 }
 
 
 void simple_uart_put(uint8_t cr) {
     NRF_UART0->TXD = (uint8_t) cr;
-
     while (NRF_UART0->EVENTS_TXDRDY != 1) {
         // Wait for TXD data to be sent
     }
-
     NRF_UART0->EVENTS_TXDRDY = 0;
 }
 
-void simple_uart_putstring(const uint8_t *str) {
+void uart_put_string(const uint8_t *str) {
     uint_fast8_t i = 0;
     uint8_t ch = str[i++];
     while (ch != '\0') {
@@ -68,12 +53,11 @@ void simple_uart_putstring(const uint8_t *str) {
 
 int main(void) {
     clock_initialization();
-    nrf_delay_ms(100);
-    UART_init();
+    nrf_delay_ms(300);
+    uart_init();
     while (1) {
         nrf_delay_ms(1000);
-        simple_uart_putstring((const uint8_t *)"\n\rMPU9150 Initialize success\n\r");
-        printf("ACC:  %d	%d	%d	", 25, 255, 28);
+        uart_put_string((const uint8_t *) "UART debug\r\n");
         NRF_P0->DIRSET = (1 << 22);
         nrf_delay_ms(1000);
         NRF_P0->DIRCLR = (1 << 22);
